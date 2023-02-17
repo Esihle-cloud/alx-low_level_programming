0dffb51f4a980aa9c1a3ca5f356c9b48ff2b3184
Tshepiso Mahoa <zhxnmahoa@gmail.com>
2023-02-16 18:26:07 -0800

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
-
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    retun (0);
+       printf("with proper grammar, but the outcome is a piece of art,\");
+                return (0);
 }
